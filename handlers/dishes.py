712119a72447d9ff3caf5092213a844e257af5f6
Nurislam2 <satybaldievnurislam248@gmail.com>
2024-08-10 13:15:08 +0600

    оставил пустые строки между фукциями

--- handlers/dishes.py
+++ handlers/dishes.py
@@ -9,10 +9,13 @@ async def drinks_handler(message: types.Message):
     image=types.FSInputFile("pictures/cocacola.jpg")
     await message.answer_photo(photo=image,caption="Coca-Cola")
 
+
 @dishes_router.message(F.text=="Завтраки")
 async def breakfast_handler(message: types.Message):
     image=types.FSInputFile("pictures/breakfast.jpg")
     await message.answer_photo(photo=image,caption="Яичница с салатом")
+
+
 @dishes_router.message(F.text=="Горячие блюда")
 async def Hot_dishes_handler(message: types.Message):
     image=types.FSInputFile("pictures/dishes.jpg")
--- handlers/start.py
+++ handlers/start.py
@@ -8,22 +8,23 @@ start_router = Router()
 @start_router.message(Command("start"))
 async def start_command_handler(message: types.Message):
     kb=types.InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            types.InlineKeyboardButton(text="Наш адрес",url="https://2gis.kg/bishkek/geo/15763234351127723/74.58716%2C42.884312?m=74.587282%2C42.884402%2F19.97"),
-            types.InlineKeyboardButton(text="Контакты", callback_data="Контакты"),
-        ],
-        [
-            types.InlineKeyboardButton(text="Наши вакансии", callback_data="Вакансии")
-        ]
-    ])
-
+        inline_keyboard=[
+            [
+                types.InlineKeyboardButton(text="Наш адрес",url="https://2gis.kg/bishkek/geo/15763234351127723/74.58716%2C42.884312?m=74.587282%2C42.884402%2F19.97"),
+                types.InlineKeyboardButton(text="Контакты", callback_data="Контакты"),
+            ],
+            [
+                types.InlineKeyboardButton(text="Наши вакансии", callback_data="Вакансии")
+            ]
+        ])
     await message.answer("Здраствуйте! Добро подаловать в Кафе Фаиза",reply_markup=kb)
+
 
 @start_router.callback_query(F.data=="Контакты")
 async def contact_handler(callback: types.CallbackQuery):
     await callback.message.answer("Администратор:0555884466"+"\n"+
                                   "Для заказа:0226448877")
+
 
 @start_router.callback_query(F.data=="Вакансии")
 async def vacancies_handler(callback: types.CallbackQuery):
